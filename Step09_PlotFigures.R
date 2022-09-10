95de13d60a20502a60b7527c2f361e64ae358d2a
jaclynrbeck <jaclyn.r.beck@gmail.com>
2022-09-09 22:09:04 -0700

    Added missing figure plotting file

---
+++ Step09_PlotFigures.R
@@ -0,0 +1,9 @@
+# Runs scripts that plot figures for the paper
+
+source(file.path("FigurePlotting", "Figure5.R"))
+source(file.path("FigurePlotting", "Figure6.R"))
+source(file.path("FigurePlotting", "Figure7.R"))
+source(file.path("FigurePlotting", "SuppFigure3.R"))
+source(file.path("FigurePlotting", "SuppFigure4.R"))
+source(file.path("FigurePlotting", "SuppFigure5.R"))
+source(file.path("FigurePlotting", "SuppFigure6.R"))
